2041290a048883354054924cd91308f08e1bb32c
Michael Drew Teter <mteter13@gmail.com>
2014-06-07 17:57:21 -0400

    the board is being created successfully

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,13 @@
 class Sudoku
 	def initialize(board_string)
+		@sudoku_board = []
+  	until board_string.length == 0
+    		row = board_string.slice!(0, 9).split("")
+    		row.map! { |num| num.to_i}
+    		@sudoku_board << row
+  	end
 	end
+  
 end
 
 my_game = Sudoku.new("000070408000401397409820065068007040740109082090600730280016903631502000909040000")
